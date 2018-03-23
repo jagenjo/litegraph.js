270941b4cf06097fbc3f0dd659dea4935091900a
Kristofer <kristofer.skyttner@skymaker.se>
2018-03-23 18:25:06 +0100

    Build 0.4.0

--- gruntfile.js
+++ gruntfile.js
@@ -20,9 +20,6 @@ module.exports = function (grunt) {
         dest: 'build/litegraph.js'
       }
     },
-    clean: {
-      build: {src: ['build/*']}
-    },
     closureCompiler: {
 
       options: {
@@ -42,28 +39,8 @@ module.exports = function (grunt) {
     }
   })
 
-  // grunt.registerTask('buildPackage', function () {
-  //   var pkg = grunt.config.data.pkg
-  //   var newPackage = {
-  //     version: pkg.version,
-  //     name: 'litegraph.js', //* Static name without ogranisation
-  //     main: 'litegraph.js',
-  //     description: pkg.description,
-  //     dependencies: pkg.dependencies,
-  //     author: pkg.author,
-  //     license: 'MIT',
-  //     scripts: {
-
-  //     }
-  //   }
-
-  //   grunt.file.write('build/package.json', JSON.stringify(newPackage, undefined, 2))
-  // })
-
   grunt.loadNpmTasks('grunt-contrib-concat')
-  grunt.loadNpmTasks('grunt-contrib-copy')
   grunt.loadNpmTasks('grunt-closure-tools')
-  grunt.loadNpmTasks('grunt-contrib-clean')
 
   grunt.registerTask('build', ['concat:build', 'closureCompiler'])
 }
