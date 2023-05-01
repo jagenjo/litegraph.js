60ade95edcf28c972d68bb7f8edd0226e0cb03d9
Michael Poutre <m1kep.my.mail@gmail.com>
2023-04-30 22:47:42 -0700

    refactor: Update startup message for example app to include URL
    Allows clicking the URL to launch example app, this also allows some IDEs such as WebStorm to launch integrated debugger far more easily

--- utils/server.js
+++ utils/server.js
@@ -7,4 +7,4 @@ app.use('/external', express.static('external'))
 app.use('/editor', express.static('editor'))
 app.use('/', express.static('editor'))
 
-app.listen(8000, () => console.log('Example app listening on port 8000!'))
+app.listen(8000, () => console.log('Example app listening on http://127.0.0.1:8000!'))
