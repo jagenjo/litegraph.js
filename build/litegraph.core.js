b5a4ce8f4f120fe793729076616c7f40ea0ddaeb
Dr.Lt.Data <dr.lt.data@gmail.com>
2023-05-12 17:00:41 +0900

    allow other actions. Just block dragging node.

--- build/litegraph.core.js
+++ build/litegraph.core.js
@@ -5831,7 +5831,7 @@ LGraphNode.prototype.executeAction = function(action)
         }
 
 		//left button mouse / single finger
-        if (e.which == 1 && !this.pointer_is_double && (!node || !node.flags || !node.flags.pinned))
+        if (e.which == 1 && !this.pointer_is_double)
 		{
             if (e.ctrlKey)
 			{
@@ -6018,6 +6018,11 @@ LGraphNode.prototype.executeAction = function(action)
                 //it wasn't clicked on the links boxes
                 if (!skip_action) {
                     var block_drag_node = false;
+
+                    if(node && node.flags && node.flags.pinned) {
+						block_drag_node = true;
+					}
+
 					var pos = [e.canvasX - node.pos[0], e.canvasY - node.pos[1]];
 
                     //widgets
