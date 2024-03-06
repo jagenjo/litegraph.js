be460cf071d7ae48fadb29d7eead1b6847d3914d
Daniel Lewis <daniel.lewis.ab@gmail.com>
2024-03-05 21:32:28 -0700

    Fixed bug in LGraphCanvas 5778 assignment to uninitialized variable

--- src/LGraphCanvas.js
+++ src/LGraphCanvas.js
@@ -5775,7 +5775,7 @@ class LGraphCanvas {
 			}else{
 				// is not not connected
 			}
-			nodeNewType = false;
+			let nodeNewType = false;
 			if(typeof slotTypesDefault[fromSlotType] == "object" || typeof slotTypesDefault[fromSlotType] == "array"){
 				for(var typeX in slotTypesDefault[fromSlotType]){
 					if (opts.nodeType == slotTypesDefault[fromSlotType][typeX] || opts.nodeType == "AUTO"){
