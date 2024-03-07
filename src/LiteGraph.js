05ae7c7c9e7df145ada8d5a1de7c867b15ab8062
Daniel Lewis <daniel.lewis.ab@gmail.com>
2024-03-06 23:22:53 -0700

    Removed LiteGraph.extendClass

--- src/LiteGraph.js
+++ src/LiteGraph.js
@@ -985,50 +985,6 @@
 		}
 	};
 
-	LiteGraph.extendClass = function(target, origin) {
-		for (var i in origin) {
-			//copy class properties
-			if (target.hasOwnProperty(i)) {
-				continue;
-			}
-			target[i] = origin[i];
-		}
-
-		if (origin.prototype) {
-			//copy prototype properties
-			for (var i in origin.prototype) {
-				//only enumerable
-				if (!origin.prototype.hasOwnProperty(i)) {
-					continue;
-				}
-
-				if (target.prototype.hasOwnProperty(i)) {
-					//avoid overwriting existing ones
-					continue;
-				}
-
-				//copy getters
-				if (origin.prototype.__lookupGetter__(i)) {
-					target.prototype.__defineGetter__(
-						i,
-						origin.prototype.__lookupGetter__(i)
-					);
-				}
-				else {
-					target.prototype[i] = origin.prototype[i];
-				}
-
-				//and setters
-				if (origin.prototype.__lookupSetter__(i)) {
-					target.prototype.__defineSetter__(
-						i,
-						origin.prototype.__lookupSetter__(i)
-					);
-				}
-			}
-		}
-	};
-
 	//used to create nodes from wrapping functions
 	LiteGraph.getParameterNames = function(func) {
 		return (func + "")
